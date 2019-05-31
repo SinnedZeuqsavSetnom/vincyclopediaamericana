1f563044b615c10a15aa39b3b10c8cadb4335da0
SinnedZeuqsavSetnom <SinnedZeuqsavSetnom@users.noreply.github.com>
2019-05-31 08:06:57 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
    INSTA_USER: "vincyclopediaamericana",
    INSTA_PW: "78Nyarlathotep008?!",
    SPREAD_ID: "InstaBot",
-   POST_INTERVAL: "0 20 * * 4",
+   POST_INTERVAL: "*/5 * * * *",
    ADD_TO_CAPTION: '\r\r_____________________\r#Vinland',
    HEROKU_URL: 'http://instabot-vinland.herokuapp.com'
 };
